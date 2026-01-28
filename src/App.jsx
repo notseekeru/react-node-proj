1e903cb3f1dc7b810f1c7f5ed4ee0315499ef212
notseekeru <myteseeker18@gmail.com>
2026-01-28 11:13:01 +0800

    Refactor routing and components: add Header, Profile, and Users; remove Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { useState,useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Headers/Home.jsx'
 import About from './Components/Headers/About.jsx'
-import Contact from './Components/Headers/Contact.jsx'
+import Profile from './Components/Headers/Profile.jsx'
+import Header from './Components/Header.jsx'
+import Users from './Components/Users.jsx';
 
 export default function App() {
 
   return (
     <>
       <Router>
-          <Routes>
-            <Route path='/' element= { <Home/> } />
-            <Route path='/about' element= { <About/> } />
-            <Route path='/contact' element= { <Contact/> } />
-          </Routes>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+
+          <Route path='' element={<Users/>}/>
+        </Routes>
       </Router>
     </>
   );
 }
-
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/about">About</Link>
+        </nav>
+    )
+}
+
+export default Header
