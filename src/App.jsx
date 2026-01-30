a0459841dd2fa9a044312a87be1501c11cc3feae
notseekeru <myteseeker18@gmail.com>
2026-01-30 17:37:01 +0800

    deleted everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import './App.css';
 import { useState,useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Headers/Home.jsx'
-import About from './Components/Headers/About.jsx'
-import Profile from './Components/Headers/Profile.jsx'
-import Header from './Components/Header.jsx'
-import Users from './Components/Users.jsx';
 
 export default function App() {
-
   return (
     <>
-      <Router>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-
-          <Route path='' element={<Users/>}/>
-        </Routes>
-      </Router>
     </>
   );
 }
